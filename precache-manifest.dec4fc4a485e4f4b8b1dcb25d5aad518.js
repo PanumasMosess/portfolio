ba848f675248731303768efe960a69e9a735eed0
Panumas Srisook <Panumas.srisook@gmail.com>
2019-06-25 19:47:59 +0700

    Updates

---
+++ precache-manifest.dec4fc4a485e4f4b8b1dcb25d5aad518.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10de49035f6e00e4383c769cb5e9ffba",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "492239b974ba0b202be6",
+    "url": "/portfolio/static/css/main.f9c67ae1.chunk.css"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/portfolio/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "492239b974ba0b202be6",
+    "url": "/portfolio/static/js/main.79bb2065.chunk.js"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  }
+]);
